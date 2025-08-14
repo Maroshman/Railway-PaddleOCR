1ff5f005bdb748975557dcd6d6230cf9ee5a6f58
Maros Hrebik <maroshman@gmail.com>
2025-08-14 16:25:47 +0200

    corrected model reference to actually include it

--- main.py
+++ main.py
@@ -18,8 +18,11 @@ def get_ocr():
     global ocr
     if ocr is None:
         from paddleocr import PaddleOCR
-        ocr = PaddleOCR(use_angle_cls=True, lang='en')
-        rec_model_dir='models/latin_PP-OCRv5_mobile_rec_infer'
+        ocr = PaddleOCR(
+            use_angle_cls=True,
+            lang='en',
+            rec_model_dir='models/latin_PP-OCRv5_mobile_rec_infer'
+        )
     return ocr
 app = FastAPI()
 
